25182fb0d71882e4f714cb7e1465fa7eebf66171
tranghaviet <viettrangha@gmail.com>
2017-06-23 16:10:28 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,31 @@
 class UsersController < ApplicationController
   def new
-  	render template: "users/new"
+  	@user = User.new
+  	# render template: "users/new" -> ko can vi Rails tu tim den template new
+  	# trong thu muc views/users
   end
 
   def show
   	@user = User.find(params[:id])
+  	# debugger
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    	# to display the message on the first page after the redirect
+      flash[:success] = "Welcome to toy app"
+      redirect_to @user # ~ redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+    	# bat buộc trong params phai có key là user
+    	# khi khong co key user thi Rails bao error -> co the dung fetch()
+    	# trong key user chi cho phép name và email, pass va pass_confirm
+    	params.require(:user).permit(:name, :email, :password, :password_confirm)
+    end
 end
